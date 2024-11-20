f2cdd77a3c9b30062d3e8e3c72d7aa99714b33b9
EmilieNirinasoa <fanomezanaemilienirinasoa@gmail.com>
2024-11-20 16:33:50 +0300

    css test#1,2,3

--- src/Components/ConversationItem.js
+++ src/Components/ConversationItem.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-export default function ConversationItem() {
+export default function ConversationItem({props}) {
   return (
-    <div>
-    ConversationItem
+    <div className='conversation-container'>
+        <p className='con-icon'>{props.name[0]}</p>
+        <p className='con-title'>{props.name}</p>
+        <p className='con-LastMessage'>{props.LastMessage}</p>
+        <p className='con-TimesTamp'>{props.TimesTamps}</p>
+   
     </div>
   );
 }
