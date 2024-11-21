0f4b4813d386fef5fd8fabcc0340b00ac459bc0f
EmilieNirinasoa <fanomezanaemilienirinasoa@gmail.com>
2024-11-21 14:55:08 +0300

    utilisation de outlet et react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 
 import './App.css';
 // import Login from './Components/Login';
+import  {Routes,Route} from 'react-router-dom'
 import MainContainer from './Components/MainContainer';
-
-
-
+import Login from './Components/Login';
+import Welcome from './Components/Welcome'
+import ChatArea from './Components/ChatArea'
+import UsersGroups from './Components/Users_Groups'
+import Users from './Components/Users'
+import CreateGroup from './Components/CreateGroup'
 function App() {
   return (
     <div className="App">
-      <MainContainer/>
+      {/* <MainContainer/> */}
+      <Routes>
+        <Route path='/' element={ <Login/>}/>
+        <Route path='app' element={ <MainContainer/>}>
+        <Route path='Welcome' element={ <Welcome/>}></Route>
+        <Route path='Chat' element={ <ChatArea/>}></Route>  
+        <Route path='Groups' element={ <UsersGroups/>}></Route> 
+        <Route path='Users' element={ <Users/>}></Route>
+        <Route path='create_Group' element={ <CreateGroup/>}></Route>      
+        </Route>
+         
+      </Routes>
       {/* <Login/> */}
     </div>
   );
