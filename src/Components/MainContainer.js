cf7bafc3a2d2814368be12cd0dddc56dcef6d7fc
EmilieNirinasoa <fanomezanaemilienirinasoa@gmail.com>
2024-11-21 07:13:01 +0300

    css du composant ChatArea

--- src/Components/MainContainer.js
+++ src/Components/MainContainer.js
@@ -3,10 +3,12 @@ import   './myStyles.css';
 import Sidebar from './Sidebar';
 import ChatArea from './ChatArea';
 export default function MainContainer() {
+  const props={name:'Test#1',TimesTamps:'12:00'}
   return (
     <div className='main-container'>
+    
           <Sidebar/>
-          <ChatArea/>
+          <ChatArea props={props}/>
     </div>
   );
 }
