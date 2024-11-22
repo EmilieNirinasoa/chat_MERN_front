289e2a7af195ad0447967c3ce747ed22925cbe04
EmilieNirinasoa <fanomezanaemilienirinasoa@gmail.com>
2024-11-22 06:34:18 +0300

    mode noir

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -3,7 +3,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import ConversationItem from './ConversationItem';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import PersonAddAlt1RoundedIcon from '@mui/icons-material/PersonAddAlt1Rounded';
 import GroupAddIcon from '@mui/icons-material/GroupAdd';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import NightlightIcon from '@mui/icons-material/Nightlight';
@@ -11,6 +11,7 @@ import { IconButton } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 export default function Sidebar() {
+    const [ligththeme,setLigthTheme]=useState(true);
     const [conversations,,setConversations]=useState([
         {
             name:'Test#1',
@@ -32,36 +33,39 @@ export default function Sidebar() {
     const navigate= useNavigate();
   return (
     <div className='sidebar-container'>
-    <div className='sb-header'>
+    <div className={'sb-header ' + ((ligththeme)? '' :'dark')}>
     <div >
-    <IconButton>
+    <IconButton className={'icon ' + ((ligththeme)? '' :'dark')}>
     <AccountCircleIcon/>
     </IconButton>
     </div>
     <div >
-    <IconButton onClick={()=>{navigate('Users')}}>
-    <PersonAddIcon/>
+    <IconButton onClick={()=>{navigate('Users')}} className={'sb-header ' + ((ligththeme)? '' :'dark')}>
+    <PersonAddAlt1RoundedIcon/>
     </IconButton>
-    <IconButton onClick={()=>{navigate('Groups')}}>
+    <IconButton onClick={()=>{navigate('Groups')}} className={'sb-header ' + ((ligththeme)? '' :'dark')}>
     <GroupAddIcon/>
     </IconButton>
-    <IconButton onClick={()=>{navigate('create_Group')}}>
+    <IconButton onClick={()=>{navigate('create_Group')}} className={'sb-header ' + ((ligththeme)? '' :'dark')}>
     <AddCircleIcon/>
     </IconButton>
-    <IconButton>
-    <NightlightIcon/>
+    <IconButton onClick={()=>{setLigthTheme((PrevValue)=>{
+        return!PrevValue;
+    })}} className={'sb-header ' + ((ligththeme)? '' :'dark')}>
+        {!ligththeme  && <NightlightIcon/>}
+        {ligththeme  && <LightModeIcon/>}
     </IconButton>
     </div>
     
     </div>
-    <div className='sb-search'>
+    <div className={'sb-search ' + ((ligththeme)? '' :'dark')}>
         <IconButton>
         <SearchIcon />
         </IconButton>
 
-    <input placeholder='search' className='search-box'/>
+    <input placeholder='search'  className={'search-box ' + ((ligththeme)? '' :'dark')}/>
     </div>
-    <div className='sb-conversation'>
+    <div  className={'sb-conversation ' + ((ligththeme)? '' :'dark')} >
     {conversations.map((conversation)=>{
         return <ConversationItem  props={conversation} key={conversation.name}  />
     })}
